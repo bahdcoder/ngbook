cbb574ccb36d9cb51168e5781deba5da0e86e508
Angular CLI <angular-cli@angular.io>
2017-07-27 04:56:37 -0700

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { NgbookPage } from './app.po';
+
+describe('ngbook App', () => {
+  let page: NgbookPage;
+
+  beforeEach(() => {
+    page = new NgbookPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
