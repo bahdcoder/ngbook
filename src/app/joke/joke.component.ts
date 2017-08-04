2096407a6d1fae7832f4344179485b3f3d724be6
frantz <frantz@bahdcasts.com>
2017-08-05 00:40:07 +0100

    can-modify

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { AuthService } from '../services/auth.service'
 
 @Component({
   selector: 'app-joke',
@@ -7,9 +8,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class JokeComponent implements OnInit {
   @Input() joke 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  canModify() : boolean { 
+    return this.joke.user.id == this.authService.getAuthUserId() 
   }
 
 }
