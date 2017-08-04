4688a058fd582fc603d387ad5de9512b9ed1959a
frantz <frantz@bahdcasts.com>
2017-08-04 01:09:09 +0100

    check-is-following

--- src/app/profile/follow/follow.component.ts
+++ src/app/profile/follow/follow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core'
 import { FollowService } from './../../services/follow.service'
 
 @Component({
@@ -6,7 +6,7 @@ import { FollowService } from './../../services/follow.service'
   templateUrl: './follow.component.html',
   styleUrls: ['./follow.component.css']
 })
-export class FollowComponent implements OnInit {
+export class FollowComponent implements OnInit, OnChanges {
   @Input() currentProfileId
   public isFollowing: boolean 
   private isLoading: boolean = true 
@@ -15,11 +15,20 @@ export class FollowComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.checkIsFollowing()
+  }
+
+  checkIsFollowing() {
     this.followService.isFollowing(this.currentProfileId)
                       .then(response => {
                         this.isFollowing = response 
                         this.isLoading = false 
                       })
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes.currentProfileId.currentValue)
+    this.checkIsFollowing()
   }
 
   follow() {
