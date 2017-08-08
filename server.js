78218e6f45ece41d45bf92a913c3e08f8896b8a9
frantz <frantz@bahdcasts.com>
2017-08-08 08:19:47 +0100

    prod\

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const path = require('path')
 const app = express() 
 
-app.use(express.static(__dirname + '/dist'))
 
+app.use(express.static(__dirname + '/dist'))
 
 app.listen(process.env.PORT || 8080)
